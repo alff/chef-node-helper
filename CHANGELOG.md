333fc964660d3350a9e0ae5c4536821b53a48115
Alex Khalkuziev <akhalkuziev@mirantis.com>
2013-07-29 15:56:36 -0700

    Add initial version of recipe

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for chef-node-helper
+
+This file is used to list changes made in each version of chef-node-helper.
+
+## 0.1.0:
+
+* Initial release of chef-node-helper
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/common.rb
@@ -0,0 +1,17 @@
+
+
+
+node.save
+
+node_cached = search(:node, "name:#{node.name} AND chef_environment:#{node.chef_environment}")
+sttime=Time.now.to_f
+until node_cached.roles.empty? || node_cached.roles.nil?
+  if (Time.now.to_f-sttime)>=60
+    Chef::Log.error "Timeout exceeded while node have been indexed.."
+    exit 1
+  else
+    Chef::Log.info "Waiting until node #{node.name} indexing."
+    sleep 5
+    node_cached = search(:node, "name:#{node.name} AND chef_environment:#{node.chef_environment}")
+  end
+end
